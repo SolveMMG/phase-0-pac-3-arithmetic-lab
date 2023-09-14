81bd5737746b4f4bce2d08654a842d19a2d73171
Your Name <mikachupichu@gmail.com>
2023-09-14 11:30:58 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(){
+    a+b 
+}
+function subtract(){
+    a-b 
+}
+function divide(){
+    a/b 
+}
+function multiply(){
+    a*b 
+}
+function add(a,b){
+    return a+b;
+}
+function divide(a,b){
+    return a/b 
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b 
+}
+function increment(a){
+    return a+1
+}
+function decrement (a){
+    return a-1
+}
+function makeInt(string){
+    return parseInt(string,10);
+}
+function preserveDecimal(string){
+    return parseFloat(string);
+}
